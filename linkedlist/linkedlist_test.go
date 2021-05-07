bd2153a0aa478aba7c203b268195c7604faf3fb8
arthurh0812 <arthurh1204@gmail.com>
2021-05-07 08:33:18 +0200

    Testing RemoveHead and Prepend

--- linkedlist/linkedlist_test.go
+++ linkedlist/linkedlist_test.go
@@ -44,3 +44,53 @@ func TestLinkedList_Append(t *testing.T) {
 		}
 	}
 }
+
+func TestLinkedList_Prepend(t *testing.T) {
+	list := Empty()
+
+	input := []int{4, 3, 6, 18, 32, 64, 37}
+	output := []int{37, 64, 32, 18, 6, 3, 4}
+
+	for _, n := range input {
+		list.Prepend(n)
+	}
+
+	got := list.Values()
+
+	if gotl, inputl := len(got), len(input); gotl != inputl {
+		t.Fatalf("lenghts do not match: len(got) = %d, len(input) = %d", gotl, inputl)
+	}
+
+	for i, n := range list.Values() {
+		if output[i] != n {
+			t.Errorf("number %d: incorrect value: %v", i+1, n)
+		}
+	}
+}
+
+func TestLinkedList_RemoveHead(t *testing.T) {
+	list := New("hello", Empty())
+
+	list.Append("moin")
+	list.Append("everything's fine")
+	list.Prepend("not so ok")
+	list.Prepend(75)
+	list.Append(true)
+	list.Prepend("i like")
+
+	v := list.RemoveHead()
+	if v != "i like" {
+		t.Errorf("incorrect head value: %v", v)
+	}
+
+	t.Log(list.String())
+
+	list.Clear() // list gets empty again
+
+	v = list.RemoveHead()
+	if v != nil {
+		t.Errorf("incorrect head value: %v", v)
+	}
+
+	t.Log(list.String())
+}
