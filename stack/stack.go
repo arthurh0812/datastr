3076858861e2e0d36f6a76eb33fae639adc72cc6
arthurh0812 <arthurh1204@gmail.com>
2021-05-07 15:57:12 +0200

    First and Last operation, improved New constructor

--- stack/stack.go
+++ stack/stack.go
@@ -6,6 +6,20 @@ type Stack struct {
 	list *linkedlist.LinkedList
 }
 
+func (s *Stack) First() interface{} {
+	if s.isEmpty() {
+		return nil
+	}
+	return s.list.Tail()
+}
+
+func (s *Stack) Last() interface{} {
+	if s.isEmpty() {
+		return nil
+	}
+	return s.list.Head()
+}
+
 func (s *Stack) isEmpty() bool {
 	return s == nil || s.list.IsEmpty()
 }
@@ -15,7 +29,7 @@ func (s *Stack) IsEmpty() bool {
 }
 
 func (s *Stack) Push(val interface{}) {
-	if s == nil {
+	if s.isEmpty() {
 		return
 	}
 	s.list.Prepend(val)
@@ -23,18 +37,10 @@ func (s *Stack) Push(val interface{}) {
 
 // Pop removes and returns the first element.
 func (s *Stack) Pop() (val interface{}) {
-	if s == nil {
+	if s.isEmpty() {
 		return
 	}
 	return s.list.RemoveHead()
-}
-
-// Peek only returns the first element.
-func (s *Stack) Peek() (val interface{}) {
-	if s == nil {
-		return nil
-	}
-	return s.list.Head()
 }
 
 func Empty() *Stack {
@@ -43,13 +49,20 @@ func Empty() *Stack {
 	}
 }
 
-func New(val interface{}) *Stack {
+func New(val interface{}, append *Stack) *Stack {
+	toAppend := linkedlist.Empty()
+	if !append.isEmpty() {
+		toAppend = append.list
+	}
 	return &Stack{
-		list: linkedlist.New(val, linkedlist.Empty()),
+		list: linkedlist.New(val, toAppend),
 	}
 }
 
 func (s *Stack) String() string {
+	if s.isEmpty() {
+		return "[]"
+	}
 	return s.list.String()
 }
 
