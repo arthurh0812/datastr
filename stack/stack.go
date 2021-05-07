f2da90d498095ea2d7e7c3b6dfbebcbed4a11383
arthurh0812 <arthurh1204@gmail.com>
2021-05-07 13:38:21 +0200

    Pop and Peek operations added, IsEmpty + improvement of Push operation

--- stack/stack.go
+++ stack/stack.go
@@ -6,8 +6,41 @@ type Stack struct {
 	list *linkedlist.LinkedList
 }
 
+func (s *Stack) isEmpty() bool {
+	return s == nil || s.list.IsEmpty()
+}
+
+func (s *Stack) IsEmpty() bool {
+	return s.isEmpty()
+}
+
 func (s *Stack) Push(val interface{}) {
+	if s == nil {
+		return
+	}
 	s.list.Prepend(val)
+}
+
+// Pop removes and returns the first element.
+func (s *Stack) Pop() (val interface{}) {
+	if s == nil {
+		return
+	}
+	return s.list.RemoveHead()
+}
+
+// Peek only returns the first element.
+func (s *Stack) Peek() (val interface{}) {
+	if s == nil {
+		return nil
+	}
+	return s.list.Head()
+}
+
+func Empty() *Stack {
+	return &Stack{
+		list: linkedlist.Empty(),
+	}
 }
 
 func New(val interface{}) *Stack {
@@ -15,3 +48,4 @@ func New(val interface{}) *Stack {
 		list: linkedlist.New(val, linkedlist.Empty()),
 	}
 }
+
