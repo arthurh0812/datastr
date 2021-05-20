82a89da071767d38497c061a96655d7b3d7faca1
arthurh0812 <arthurh1204@gmail.com>
2021-05-20 19:05:39 +0200

    Hash table data concrete data structure: size, capacity, table (array of linked lists), isOutOfBounds, normalizeIndex and first implementation of Insert operation

--- bst/traversals.go
+++ bst/traversals.go
@@ -5,6 +5,8 @@ import (
 	"github.com/arthurh0812/datastruct/stack"
 )
 
+// PreOrder is a traversal operation that iteratively calls the cb function on each each
+// top node, then traverses the left subtree of that node and after that its right subtree.
 func (t *Tree) PreOrder(cb func(node *Node)) {
 	nodeStack := stack.New(t.root, stack.Empty())
 	for !nodeStack.IsEmpty() {
@@ -19,7 +21,8 @@ func (t *Tree) PreOrder(cb func(node *Node)) {
 	}
 }
 
-// InOrder traverses through the binary tree in sorting order.
+// InOrder is a traversal operation that iteratively calls the cb function on each node
+// of the tree in increasing order.
 func (t *Tree) InOrder(cb func(node *Node)) {
 	trav := t.root
 	nodeStack := stack.Empty()
@@ -35,7 +38,9 @@ func (t *Tree) InOrder(cb func(node *Node)) {
 	}
 }
 
-// PostOrder is a more complex traversal.
+// PostOrder is a traversal operation that iteratively traverses the left and then the right
+// subtree calling the cb on each node, and only after those two operations calls the cb
+// function on the top node.
 func (t *Tree) PostOrder(cb func(node *Node)) {
 	trav, prev := t.root, (*Node)(nil)
 	nodeStack := stack.Empty()
@@ -44,18 +49,21 @@ func (t *Tree) PostOrder(cb func(node *Node)) {
 			nodeStack.Push(trav)
 			trav = trav.left
 			continue
-		}
+		} // up to this point trav must be nil here
 		last := nodeStack.Last().(*Node)
-		if last.right == nil || last.right == prev {
-			nodeStack.Pop()
-			cb(last)
-			prev, trav = last, nil
-		} else {
+		if last.right != nil && last.right != prev {
+			// go right if there is a right node and if that node is not the previously most recently popped node
 			trav = last.right
+			continue
 		}
+		nodeStack.Pop()
+		cb(last)
+		prev, trav = last, nil
 	}
 }
 
+// LevelOrder is a BFS-algorithm that iteratively calls the given cb function on each
+// traversed node, from left to right, level by level.
 func (t *Tree) LevelOrder(cb func(node *Node)) {
 	nodeQueue := queue.New(t.root, queue.Empty())
 	for !nodeQueue.IsEmpty() {
