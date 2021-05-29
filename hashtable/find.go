0eaf4a89270928bcc81dd3a33df3f393e2c9260b
arthurh0812 <arthurh1204@gmail.com>
2021-05-29 21:46:02 +0200

    ContainsValue, Contains and GetKey operations

--- hashtable/find.go
+++ hashtable/find.go
@@ -6,7 +6,15 @@ func (h *HashTable) find(key types.Value) *Entry {
 	return h.table.getEntry(key, h.fn)
 }
 
-func (h *HashTable) Get(key types.Value) interface{} {
+func (h *HashTable) findValue(val interface{}) *Entry {
+	return h.table.getEntryByValue(val)
+}
+
+func (h *HashTable) Get(key types.Value) (val interface{}) {
+	return h.get(key)
+}
+
+func (h *HashTable) get(key types.Value) interface{} {
 	if key == nil {
 		return nil
 	}
@@ -15,4 +23,28 @@ func (h *HashTable) Get(key types.Value) interface{} {
 		return nil
 	}
 	return e.Val
+}
+
+func (h *HashTable) GetKey(val interface{}) types.Value {
+	return h.getKey(val)
+}
+
+func (h *HashTable) getKey(val interface{}) types.Value {
+	e := h.findValue(val)
+	if e == nil {
+		return nil
+	}
+	return e.Key
+}
+
+func (h *HashTable) Contains(key types.Value) bool {
+	return h.contains(key)
+}
+
+func (h *HashTable) contains(key types.Value) bool {
+	return key != nil && h.find(key) != nil
+}
+
+func (h *HashTable) ContainsValue(val types.Value) bool {
+	return h.findValue(val) != nil
 }
