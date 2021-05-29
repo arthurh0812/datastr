b88c3f2a2e4a350b92782d73d248ce956cc23e8b
arthurh0812 <arthurh1204@gmail.com>
2021-05-29 21:47:53 +0200

    Constructor functions now using the new specialized hash functions

--- hashtable/constructor.go
+++ hashtable/constructor.go
@@ -1,26 +1,36 @@
 package hashtable
 
-import "github.com/arthurh0812/datastruct/linkedlist"
+import (
+	"errors"
+	"github.com/arthurh0812/datastruct/linkedlist"
+)
 
 var DefaultCapacity int64 = 3
 var DefaultLoadFactor float64 = 0.75
 
+var ErrInvalidKeyType = errors.New("the provided key type is not valid")
+
+// Default creates and returns a new default hash map (= map[string]interface{})
 func Default() *HashTable {
 	h := &HashTable{}
 	h.setTable(make([]*linkedlist.LinkedList, DefaultCapacity, 0))
 	h.setCapacity(DefaultCapacity)
 	h.setLoadFactor(DefaultLoadFactor)
-	h.setFunction(DefaultFunction)
+	h.setFunction(GetFunction("string"))
 	return h
 }
 
-func New(cap int64) (*HashTable, error) {
+func New(keyType string, cap int64) (*HashTable, error) {
 	h := &HashTable{}
 	err := h.initCapacity(cap)
 	if err != nil {
 		return nil, err
 	}
-	h.setFunction(DefaultFunction)
+	fn := GetFunction(keyType)
+	if fn == nil {
+		return nil, ErrInvalidKeyType
+	}
+	h.setFunction(fn)
 	h.setTable(make([]*linkedlist.LinkedList, cap, 0))
 	h.setLoadFactor(DefaultLoadFactor)
 	return h, nil
